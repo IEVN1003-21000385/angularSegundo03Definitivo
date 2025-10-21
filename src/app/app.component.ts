b385a3159185d925879abc5330c37dcdce8c6747
Hernandez Hugo <21000385@alumnos.utleon.edu.mx>
2025-10-21 15:49:04 -0600

    formulario Zodiaco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DistanciaComponent } from './formularios/distancia/distancia.component';
+import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent],
+  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
