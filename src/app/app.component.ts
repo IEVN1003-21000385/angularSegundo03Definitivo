f0194334c2e5e984a5425c4bd3b9109cbb44d86f
Hernandez Hugo <21000385@alumnos.utleon.edu.mx>
2025-10-22 18:28:49 -0600

    formulario axb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DistanciaComponent } from './formularios/distancia/distancia.component';
 import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
+import { AxbComponent } from './formularios/axb/axb.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent],
+  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
