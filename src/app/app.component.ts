b0c666bfd3e8765e179a5877a6196290aac3ef0a
Hernandez Hugo <21000385@alumnos.utleon.edu.mx>
2025-10-24 03:27:31 -0600

    rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { DistanciaComponent } from './formularios/distancia/distancia.component';
 import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
 import { AxbComponent } from './formularios/axb/axb.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent],
+  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angularSegundo03Definitivo';
+export class AppComponentimplements implements OnInit {
+  title = 'AngularSegundo03';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'formularios',
+        loadChildren:() => import('./formularios/formularios.routes').then(m=>m.default),
+    },
+
+];
