9f3d5a5157574776020b41eab4090bc2aaa3340f
Hernandez Hugo <21000385@alumnos.utleon.edu.mx>
2025-10-24 13:29:13 -0600

    Input y Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,19 @@ import { DistanciaComponent } from './formularios/distancia/distancia.component'
 import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
 import { AxbComponent } from './formularios/axb/axb.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TempComponent } from './tem/temp/temp.component';
+
 
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent, NavbarComponent],
+  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent, NavbarComponent, TempComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponentimplements implements OnInit {
+export class AppComponent implements OnInit {
   title = 'AngularSegundo03';
 
   ngOnInit(): void {
